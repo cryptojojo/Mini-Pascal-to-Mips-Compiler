eec9d79ca5a6fd610d84c46b254f6091b369a3bb
Crypto Jojo <cryptojojo1@gmail.com>
2018-02-08 23:46:32 -0600

    All tests done and passing

--- src/parser/SymbolTableTest.java
+++ src/parser/SymbolTableTest.java
@@ -29,19 +29,92 @@ public class SymbolTableTest {
 
 	@Test
 	public void addTest() {
+
+		// adding identifiers to the hashmap with their corresponding types
 		table.add("programName", SymbolType.PROGRAMTYPE);
 		table.add("variableName", SymbolType.VARIABLETYPE);
 		table.add("functionName", SymbolType.FUNCTIONTYPE);
 		table.add("procedureName", SymbolType.PROCEDURETYPE);
 		table.add("arrayName", SymbolType.ARRAYTYPE);
 
+		// should all yield true
 		assertTrue(table.isProgramName("programName"));
 		assertTrue(table.isVariableName("variableName"));
 		assertTrue(table.isFunctionName("functionName"));
 		assertTrue(table.isProcedureName("procedureName"));
 		assertTrue(table.isArrayName("arrayName"));
 
-		table.printOut();
+		// should all yield false - type being tested for is not correct
+		assertFalse(table.isProgramName("arrayName"));
+		assertFalse(table.isVariableName("functionName"));
+		assertFalse(table.isFunctionName("variableName"));
+		assertFalse(table.isProcedureName("programName"));
+		assertFalse(table.isArrayName("procedureName"));
+
+	}
+
+	@Test
+	public void addProgramNameTest() {
+
+		table.addProgramName("programName");
+
+		// should yield true
+		assertTrue(table.isProgramName("programName"));
+
+		// should yield false
+		assertFalse(table.isArrayName("programName"));
+
+	}
+
+	@Test
+	public void addVariableNameTest() {
+
+		table.addVariableName("variableName");
+
+		// should yield true
+		assertTrue(table.isVariableName("variableName"));
+
+		// should yield false
+		assertFalse(table.isProgramName("variableName"));
+
+	}
+
+	@Test
+	public void addFunctionNameTest() {
+
+		table.addFunctionName("functionName");
+
+		// should yield true
+		assertTrue(table.isFunctionName("functionName"));
+
+		// should yield false
+		assertFalse(table.isProcedureName("functionName"));
+
+	}
+
+	@Test
+	public void addProcedureNameTest() {
+
+		table.addProcedureName("procedureName");
+
+		// should yield true
+		assertTrue(table.isProcedureName("procedureName"));
+
+		// should yield false
+		assertFalse(table.isProgramName("procedureName"));
+
+	}
+
+	@Test
+	public void addArrayNameTest() {
+
+		table.addArrayName("arrayName");
+
+		// should yield true
+		assertTrue(table.isArrayName("arrayName"));
+
+		// should yield false
+		assertFalse(table.isProgramName("arrayName"));
 
 	}
 
