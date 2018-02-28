9f21bc39df44cf52e8fe23f71cd8f72c9cf3f801
Crypto Jojo <cryptojojo1@gmail.com>
2018-02-27 18:30:07 -0600

    Added to string for subprogram head nose

--- src/syntaxtree/SubProgramHeadNode.java
+++ src/syntaxtree/SubProgramHeadNode.java
@@ -2,7 +2,7 @@ package syntaxtree;
 
 import java.util.ArrayList;
 
-public class SubProgramHeadNode {
+public class SubProgramHeadNode extends SyntaxTreeNode {
 
 	private final String name;
 	private final String[] args;
@@ -11,6 +11,18 @@ public class SubProgramHeadNode {
 		this.name = name;
 		this.args = new String[arguments.size()];
 		arguments.toArray(this.args);
+	}
+
+	@Override
+	public String indentedToString(int level) {
+		String answer = this.indentation(level);
+		answer += "Function name: " + name + '\n';
+
+		for (int i = 0; i <= args.length; i++)
+			answer += args[i].indentedToString(level + 1);
+
+		return answer;
+
 	}
 
 }
