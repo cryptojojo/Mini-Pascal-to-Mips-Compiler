022253de872a75344a26d1cf9abff5953693076d
Crypto Jojo <cryptojojo1@gmail.com>
2018-02-27 18:21:11 -0600

    Added constructer to subprogram head node

--- src/syntaxtree/SubProgramHeadNode.java
+++ src/syntaxtree/SubProgramHeadNode.java
@@ -1,5 +1,16 @@
 package syntaxtree;
 
+import java.util.ArrayList;
+
 public class SubProgramHeadNode {
+
+	private final String name;
+	private final String[] args;
+
+	public SubProgramHeadNode(String name, ArrayList<String> arguments) {
+		this.name = name;
+		this.args = new String[arguments.size()];
+		arguments.toArray(this.args);
+	}
 
 }
