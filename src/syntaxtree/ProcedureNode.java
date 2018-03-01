2c21e46f3f16bfb9d766a8c4ac8f0ebc13b3bf00
Crypto Jojo <cryptojojo1@gmail.com>
2018-02-28 20:51:20 -0600

    Added content to procedure node, including constructers, functions to add a list of expression nodes and one to list a single expression node - also a getName and a setName

--- src/syntaxtree/ProcedureNode.java
+++ src/syntaxtree/ProcedureNode.java
@@ -1,11 +1,41 @@
 package syntaxtree;
 
+import java.util.ArrayList;
+
 public class ProcedureNode extends StatementNode {
+
+	private String name;
+	private ArrayList<ExpressionNode> args = new ArrayList<>();
+
+	public ProcedureNode(String name) {
+		this.name = name;
+	}
+
+	public void addAllExpressions(ArrayList<ExpressionNode> input) {
+		args.addAll(input);
+	}
+
+	public void addArg(ExpressionNode exp) {
+		args.add(exp);
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	@Override
 	public String indentedToString(int level) {
-		// TODO Auto-generated method stub
-		return null;
+		String answer = this.indentation(level);
+		answer += "Procedure: ";
+		answer += this.name + "\n";
+		for (ExpressionNode exp : args) {
+			answer += exp.indentedToString(level + 1);
+		}
+		return answer.toString();
 	}
 
 }
