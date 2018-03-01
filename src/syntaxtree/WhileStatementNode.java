026f42b36f66f7684a686341dc5a3e0e224cfb2d
Crypto Jojo <cryptojojo1@gmail.com>
2018-02-28 19:50:17 -0600

    Filled the indentedToString in the while statement node

--- src/syntaxtree/WhileStatementNode.java
+++ src/syntaxtree/WhileStatementNode.java
@@ -5,8 +5,6 @@ public class WhileStatementNode extends StatementNode {
 	private ExpressionNode test; // The test to be checked on each iteration of the while loop
 	private StatementNode doW; // The statement to be executed on each iteration
 
-	
-	
 	public void setTest(ExpressionNode test) {
 		this.test = test;
 	}
@@ -16,18 +14,20 @@ public class WhileStatementNode extends StatementNode {
 	}
 
 	public ExpressionNode getTest() {
-        return test;
-    }
-	
+		return test;
+	}
+
 	public StatementNode getDo() {
-        return doW;
-    }
-	
-	
+		return doW;
+	}
+
 	@Override
 	public String indentedToString(int level) {
-		// TODO Auto-generated method stub
-		return null;
+		String answer = this.indentation(level);
+		answer += "While:\n";
+		answer += this.test.indentedToString(level + 1);
+		answer += this.indentation(level) + "Do:\n" + this.doW.indentedToString(level + 1);
+		return answer;
 	}
 
 }
