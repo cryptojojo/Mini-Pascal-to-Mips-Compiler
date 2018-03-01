9fa441e63c1dd3bba757c4980615117006bedcb6
Crypto Jojo <cryptojojo1@gmail.com>
2018-02-28 20:00:57 -0600

    Added content to write node - basically the exact same as read node but used expression node instead of variable node for the content

--- src/syntaxtree/WriteNode.java
+++ src/syntaxtree/WriteNode.java
@@ -2,10 +2,21 @@ package syntaxtree;
 
 public class WriteNode extends StatementNode {
 
-	@Override
-	public String indentedToString(int level) {
-		// TODO Auto-generated method stub
-		return null;
+	private ExpressionNode content;
+
+	public WriteNode(ExpressionNode content) {
+		this.content = content;
 	}
 
+	public ExpressionNode getContent() {
+		return content;
+	}
+
+	@Override
+	public String indentedToString(int level) {
+		String answer = this.indentation(level);
+		answer += "Write\n";
+		answer += this.content.indentedToString(level + 1);
+		return answer;
+	}
 }
