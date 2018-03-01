3722a35ff6f2cd04c64c73e46526ba3e830a6bed
Crypto Jojo <cryptojojo1@gmail.com>
2018-02-28 20:26:00 -0600

    Added content to array node - two contrcutors one that takes in just a string (id) and one that takes in a string and a type

--- src/syntaxtree/ArrayNode.java
+++ src/syntaxtree/ArrayNode.java
@@ -1,10 +1,41 @@
 package syntaxtree;
 
+import scanner.TokenType;
+
 public class ArrayNode extends VariableNode {
+
+	private ExpressionNode exp;
+	private TokenType type = null;
+
+	public ArrayNode(String attr, TokenType type) {
+		super(attr);
+		this.type = type;
+	}
 
 	public ArrayNode(String attr) {
 		super(attr);
-		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return (super.getName());
+	}
+
+	public ExpressionNode getExpNode() {
+		return this.exp;
+	}
+
+	public void setExpNode(ExpressionNode input) {
+		this.exp = input;
+	}
+
+	@Override
+	public String indentedToString(int level) {
+		String answer = this.indentation(level);
+		answer += "Array: " + super.getName() + ", Type: " + type + "\n";
+		if (exp != null) {
+			answer += this.exp.indentedToString(level + 1);
+		}
+		return answer;
 	}
 
 }
