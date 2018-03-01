1cf019233d6cf9c483c7c0c0911e72879193f69d
Crypto Jojo <cryptojojo1@gmail.com>
2018-02-28 19:57:18 -0600

    Added a constucter and a getName in the read node

--- src/syntaxtree/ReadNode.java
+++ src/syntaxtree/ReadNode.java
@@ -2,6 +2,16 @@ package syntaxtree;
 
 public class ReadNode extends StatementNode {
 
+	private VariableNode name;
+
+	public ReadNode(VariableNode name) {
+		this.name = name;
+	}
+
+	public VariableNode getName() {
+		return name;
+	}
+
 	@Override
 	public String indentedToString(int level) {
 		// TODO Auto-generated method stub
