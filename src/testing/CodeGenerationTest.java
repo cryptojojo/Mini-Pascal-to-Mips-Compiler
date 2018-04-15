f8c8a8f7d2373efc8ba3e969201f4b51f009f08c
Crypto Jojo <cryptojojo1@gmail.com>
2018-04-14 20:36:49 -0500

    Succesful testing for if statement when statement is true and when it is false

--- src/testing/CodeGenerationTest.java
+++ src/testing/CodeGenerationTest.java
@@ -24,14 +24,17 @@ public class CodeGenerationTest {
 	@Test
 	public void test() {
 
+		//
 		// Testing for basic assignment (bitcoin example)
+		//
+
 		Parser parse = new Parser("test/codegen/assignment_bitcoin.pas", true);
 		SemanticAnalysis semAnalysis = new SemanticAnalysis(parse.program(), parse.getSymbolTable());
 		ProgramNode progNode = semAnalysis.analyze();
 		CodeGeneration codeGen = new CodeGeneration(progNode);
 		codeGen.generate();
 		String assembly = codeGen.getAsmCode();
-		System.out.println(assembly);
+
 		String expectedResult = ".data\n" + "\n" + "dollars : .word 0\n" + "yen : .word 0\n" + "bitcoins : .word 0\n"
 				+ "newLine: .asciiz \"\\n\"\n" + "\n" + ".text\n" + "\n" + "main:\n" + "\n" + "#Assignment Statement\n"
 				+ "\n" + "#Expression statement\n" + "li   $s0,   10000\n" + "sw  $s0,   dollars\n" + "\n"
@@ -50,17 +53,52 @@ public class CodeGenerationTest {
 				+ "la   $a0, newLine\n" + "syscall\n" + "\n" + "\n" + "#Exit Program \n" + "li  $v0, 10 \n";
 		assertEquals(expectedResult, assembly);
 
-		
-		
-		
-		
+		//
+		// testing for an if statement where statement is false
+		//
+
 		parse = new Parser("test/codegen/if_statement_F.pas", true);
-		
-		
-		
-		
-		
-		
+		semAnalysis = new SemanticAnalysis(parse.program(), parse.getSymbolTable());
+		progNode = semAnalysis.analyze();
+		codeGen = new CodeGeneration(progNode);
+		codeGen.generate();
+		assembly = codeGen.getAsmCode();
+
+		expectedResult = ".data\n" + "\n" + "uno : .word 0\n" + "newLine: .asciiz \"\\n\"\n" + "\n" + ".text\n" + "\n"
+				+ "main:\n" + "\n" + "#Assignment Statement\n" + "\n" + "#Expression statement\n" + "li   $s0,   10\n"
+				+ "sw  $s0,   uno\n" + "\n" + "# If statement\n" + "\n" + "#Expression statement\n" + "lw   $s0,  uno\n"
+				+ "\n" + "#Expression statement\n" + "li   $s1,   20\n" + "ble   $s0,   $s1,   else0\n" + "\n"
+				+ "#Write Statement\n" + "\n" + "#Expression statement\n" + "li   $s0,   10\n"
+				+ "addi   $v0,   $zero,   1\n" + "add   $a0,   $s0,   $zero\n" + "syscall\n" + "li   $v0,   4\n"
+				+ "la   $a0, newLine\n" + "syscall\n" + "j  endIf0\n" + "else0:\n" + "\n" + "#Write Statement\n" + "\n"
+				+ "#Expression statement\n" + "li   $s1,   20\n" + "addi   $v0,   $zero,   1\n"
+				+ "add   $a0,   $s1,   $zero\n" + "syscall\n" + "li   $v0,   4\n" + "la   $a0, newLine\n" + "syscall\n"
+				+ "endIf0:\n" + "\n" + "\n" + "#Exit Program \n" + "li  $v0, 10 \n";
+		assertEquals(expectedResult, assembly);
+
+		//
+		// testing for an if statement where statement is true
+		//
+
+		parse = new Parser("test/codegen/if_statement_T.pas", true);
+		semAnalysis = new SemanticAnalysis(parse.program(), parse.getSymbolTable());
+		progNode = semAnalysis.analyze();
+		codeGen = new CodeGeneration(progNode);
+		codeGen.generate();
+		assembly = codeGen.getAsmCode();
+
+		expectedResult = ".data\n" + "\n" + "uno : .word 0\n" + "newLine: .asciiz \"\\n\"\n" + "\n" + ".text\n" + "\n"
+				+ "main:\n" + "\n" + "#Assignment Statement\n" + "\n" + "#Expression statement\n" + "li   $s0,   10\n"
+				+ "sw  $s0,   uno\n" + "\n" + "# If statement\n" + "\n" + "#Expression statement\n" + "lw   $s0,  uno\n"
+				+ "\n" + "#Expression statement\n" + "li   $s1,   5\n" + "ble   $s0,   $s1,   else0\n" + "\n"
+				+ "#Write Statement\n" + "\n" + "#Expression statement\n" + "li   $s0,   10\n"
+				+ "addi   $v0,   $zero,   1\n" + "add   $a0,   $s0,   $zero\n" + "syscall\n" + "li   $v0,   4\n"
+				+ "la   $a0, newLine\n" + "syscall\n" + "j  endIf0\n" + "else0:\n" + "\n" + "#Write Statement\n" + "\n"
+				+ "#Expression statement\n" + "li   $s1,   20\n" + "addi   $v0,   $zero,   1\n"
+				+ "add   $a0,   $s1,   $zero\n" + "syscall\n" + "li   $v0,   4\n" + "la   $a0, newLine\n" + "syscall\n"
+				+ "endIf0:\n" + "\n" + "\n" + "#Exit Program \n" + "li  $v0, 10 \n";
+		assertEquals(expectedResult, assembly);
+
 	}
 
 }
